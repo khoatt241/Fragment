aab91562f4b768c1ac17ea4ce258db5cf57a6eb8
khoatt241 <khoatt241@gmail.com>
2019-08-12 20:40:56 +0700

    1

--- app/src/main/java/com/example/fragment/AndroidFragment.java
+++ app/src/main/java/com/example/fragment/AndroidFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,11 +16,14 @@ import android.view.ViewGroup;
  */
 public class AndroidFragment extends Fragment {
 
+    TextView txtFA;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_android, container, false);
+        txtFA = v.findViewById(R.id.textviewFragmentAndroid);
+        txtFA.setText("Hehe");
         return v;
     }
 
--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnAddFragment;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //gọi lại thằng quản lý, trong activity chỉ có duy nhất 1 thằng quản lý, ko được getsupport ở thời điểm khác
+        fragmentManager = getSupportFragmentManager();
+        btnAddFragment = findViewById(R.id.buttonAddFragment);
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //thực thi những yêu cầu từ Activity ví dụ như thêm, xóa, sửa
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                //Khởi tạo Fragment
+                AndroidFragment androidFragment = new AndroidFragment();
+                //khi xử dụng replace tất cả fragment bên trong sẽ bị đẩy ra hết chỉ còn duy nhất thằng mới vào
+                fragmentTransaction.replace(R.id.frameContain, androidFragment);
+                fragmentTransaction.commit();
+                //kiểm tra số lượng fragment được thêm vào
+                Log.d("BBB", fragmentManager.getFragments().size() + "");
+            }
+        });
     }
 }
