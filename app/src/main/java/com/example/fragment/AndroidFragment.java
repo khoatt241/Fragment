660fec74a5dace49b69ca110dfef1e349b5fb9ff
khoatt241 <khoatt241@gmail.com>
2019-08-12 20:15:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment/AndroidFragment.java
@@ -0,0 +1,26 @@
+package com.example.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AndroidFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_android, container, false);
+        return v;
+    }
+
+}
